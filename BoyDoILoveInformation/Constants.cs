e9c33af686dccec7045d3c12dec9cc3147e7f608
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-12-12 15:51:01 +0100

    Initial commit

---
+++ BoyDoILoveInformation/Constants.cs
@@ -0,0 +1,8 @@
+namespace BoyDoILoveInformation;
+
+public static class Constants
+{
+    public const string PluginGuid    = "hansolo1000falcon.boydoiloveinformation";
+    public const string PluginName    = "BoyDoILoveInformation";
+    public const string PluginVersion = "1.0.0";
+}
---
+++ BoyDoILoveInformation/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BoyDoILoveInformation;
+
+[BepInPlugin(Constants.PluginGuid, Constants.PluginName, Constants.PluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string GorillaInfoEndPointURL =
+            "https://raw.githubusercontent.com/HanSolo1000Falcon/GorillaInfo/main/";
+
+    public static Dictionary<string, string> KnownCheats;
+    public static Dictionary<string, string> KnownMods;
+
+    public static AssetBundle BDILIBundle;
+    
+    private void Start()
+    {
+        new Harmony(Constants.PluginGuid).PatchAll();
+        GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+    }
+
+    private void OnGameInitialized() { }
+}
